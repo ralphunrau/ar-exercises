3cf0c4851b9d40d9737d48c9b014cada8a04474b
Ralph Unrau <xralphunrau@gmail.com>
2022-05-04 01:46:56 +0000

    joined employees to stores and created new employees

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,18 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@all_stores = Store.all
+
+sum = 0
+@all_stores.each do |s|
+  sum += s.annual_revenue
+end
+
+puts "The total annual revenue is #{sum}"
+
+puts "The average annual revenue is #{sum / @all_stores.count}"
+
+@rich_stores = Store.where('annual_revenue > 1000000')
+
+puts "There are #{@rich_stores.count} stores that make over 1 million a year"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,69 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(
+  first_name: 'Brendan',
+  last_name: 'Funk',
+  hourly_rate: 47
+)
+
+@store1.employees.create(
+  first_name: 'Ethan',
+  last_name: 'Loewen',
+  hourly_rate: 159
+)
+
+@store1.employees.create(
+  first_name: 'Ralph',
+  last_name: 'Unrau',
+  hourly_rate: 47
+)
+
+@store2.employees.create(
+  first_name: 'Jacob',
+  last_name: 'Funk',
+  hourly_rate: 159
+)
+
+@store2.employees.create(
+  first_name: 'Kieren',
+  last_name: 'Steu',
+  hourly_rate: 47
+)
+
+@store2.employees.create(
+  first_name: 'Luke',
+  last_name: 'Ouelette',
+  hourly_rate: 159
+)
+
+@store2.employees.create(
+  first_name: 'Karn',
+  last_name: 'Bansi',
+  hourly_rate: 47
+)
+
+@store2.employees.create(
+  first_name: 'Kimberly',
+  last_name: 'Eaves',
+  hourly_rate: 159
+)
+
+@store2.employees.create(
+  first_name: 'Hunter',
+  last_name: 'Klaasen',
+  hourly_rate: 47
+)
+
+@store2.employees.create(
+  first_name: 'Aman',
+  last_name: 'Bola',
+  hourly_rate: 159
+)
+
+@store2.employees.create(
+  first_name: 'Hedge',
+  last_name: 'Grewal',
+  hourly_rate: 47
+)
